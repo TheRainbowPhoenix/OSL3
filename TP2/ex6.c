04bc1a3274b56b9a27afcda69786be73bcc1f915
Pho3 <rainbowdashtheone@gmail.com>
2018-11-11 23:38:17 +0100

    added job control and module toggle

--- TP2/ex6.c
+++ TP2/ex6.c
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <string.h>
+#include <termios.h>
 
 #define WAIT_ANY (-1)
 
@@ -16,7 +17,13 @@ int run = 1;
 
 char **ENV;
 
-pid_t last;
+pid_t _pgid;
+struct termios _tmodes;
+
+int _term;
+int _itty;
+
+int putErr(char * err);
 
 /* Temporary confing vars */
 
@@ -24,11 +31,91 @@ int SHOW_CWD = 1;
 int SHOW_NAME = 1;
 int SHOW_GIT = 0;
 
+char * _DEFINED_MODULES[] = {"CWD", "NAME", "GIT",0};
+
+/* Modules functions */
+
+int isModuleActive(char * module) {
+  if(strcmp(module, _DEFINED_MODULES[0]) == 0) return SHOW_CWD;
+  else if(strcmp(module, _DEFINED_MODULES[1]) == 0) return SHOW_NAME;
+  else if(strcmp(module, _DEFINED_MODULES[2]) == 0) return SHOW_GIT;
+  else return -1;
+}
+
+void toggleModule(char * module) {
+  if(strcmp(module, _DEFINED_MODULES[0]) == 0) SHOW_CWD=!SHOW_CWD;
+  else if(strcmp(module, _DEFINED_MODULES[1]) == 0) SHOW_NAME=!SHOW_NAME;
+  else if(strcmp(module, _DEFINED_MODULES[2]) == 0) SHOW_GIT=!SHOW_GIT;
+  else putErr("Module not found");
+}
+
+/* Ends here */
+
 struct cmd {
   char **argv;
 };
 
-int notBuiltIn(struct cmd *line);
+typedef struct bgJob {
+  struct bgJob *next;
+  pid_t pid;
+  char * line;
+} bgJob;
+
+bgJob * bgJobs = NULL;
+
+/* bgJob functions */
+
+
+int killJob(pid_t pid) {
+  if(getpgid(pid) < 0) return 0;
+  kill(pid, SIGINT);
+  return 1;
+}
+
+int removeBgJob(pid_t pid) {
+  bgJob *b;
+  if(bgJobs == NULL) return 0;
+  killJob(pid);
+  if(bgJobs->next == NULL && bgJobs->pid == pid) {
+    free(bgJobs->line);
+    bgJobs = NULL;
+    return 1;
+  }
+  if(bgJobs->pid == pid && bgJobs->next != NULL) {
+    free(bgJobs->line);
+    bgJobs = bgJobs->next;
+    return 1;
+  }
+  for(b=bgJobs; b!=NULL && b->next!=NULL; b=b->next) {
+    if(b->next->pid == pid) {
+      free(b->next->line);
+      b->next = b->next->next;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void updateJobs() {
+  bgJob *b;
+  for(b=bgJobs; b!=NULL; b=b->next) {
+    if(getpgid(b->pid) < 0) removeBgJob(b->pid);
+  }
+}
+
+void killAllJobs() {
+  bgJob *b;
+  int i = 1;
+  for(b=bgJobs; b!=NULL; b=b->next, i++) {
+    killJob(b->pid);
+    printf("[%d] : Stopped\n", i);
+  }
+  updateJobs();
+}
+
+/* Ends here */
+
+int notBuiltIn(char **argv);
 
 void putstr(char *s) {
   while (*s) {
@@ -40,6 +127,7 @@ void _putchar(char c) {
 }
 
 void terminate() {
+  killAllJobs();
   putstr("[39m[49m\n");
   free(ENV);
   exit(0);
@@ -78,12 +166,13 @@ void chldHandler(int sig) {
 		//printf("%d %d %d\n", pid,getpgrp(),  getpid());
 
 		if(WIFEXITED(status)) {
-			printf("[%d] Terminated (Status: %d)\n", pid, WEXITSTATUS(status));
+			printf("[%d] Done (Status: %d)\n", pid, WEXITSTATUS(status));
+      removeBgJob(pid);
 			return;
 		}
 
 		if(WIFSIGNALED(status)) {
-			printf("[%d] Terminated (Signal: %d)\n", pid, WTERMSIG(status));
+			printf("[%d] Done (Signal: %d)\n", pid, WTERMSIG(status));
 			return;
 		}
 		if(WIFSTOPPED(status)) {
@@ -265,30 +354,193 @@ int changeDir(char * path) {
   }
 }
 
+int runCmd(struct cmd *line, pid_t pgid, int _in, int _out, int fg) {
+  pid_t pid;
+  int isBuiltin;
+
+  if((isBuiltin = notBuiltIn(line->argv))<=0) {
+    return isBuiltin;
+  }
+  if(_itty) {
+    pid = getpid();
+    if(pgid==0) pgid = pid;
+    setpgid(pid, pgid);
+    if(fg) tcsetpgrp(_term, pgid);
+
+    signal(SIGINT, SIG_DFL);
+    signal(SIGQUIT, SIG_DFL);
+    signal(SIGTSTP, SIG_DFL);
+    signal(SIGTTIN, SIG_DFL);
+    signal(SIGTTOU, SIG_DFL);
+    signal(SIGCHLD, SIG_DFL);
+  }
+  if(_in != STDIN_FILENO) {
+    dup2(_in, STDIN_FILENO);
+    close(_in);
+  }
+  if(_out != STDOUT_FILENO) {
+    dup2(_out, STDOUT_FILENO);
+    close(_out);
+  }
+
+  return execvp(line->argv[0], (char * const *)line->argv);
+  perror("Exec format error");
+  exit(1);
+}
+
+int bgCmd(char **args) {
+  if(!args[1] || strcmp(args[1], "--help") == 0) {
+    putstr("[47m[90m ");
+    putstr("bg");
+    putstr(" [37m[49m ");
+    putstr("<command> Execute in backgound");
+    _putchar('\n');
+    return 0;
+  }
+  //runProcess(process *p, pid_t pgid, int _in, int _out, int _err, int fg)
+  //runJjob(Jjob *j, 0)
+  return 0;
+}
+
+/*
+typedef struct job {
+  struct job *next;
+  char * last;
+  struct cmd * command;
+  pid_t pgid;
+  char completed;
+  char stopped;
+  int status;
+} job;
+
+ */
+
+/*void bgJob(job *j, int c) {
+  if(c) {
+    if(kill(- j->pgid, SIGCONT) < 0) perror("SIGCONT error cannot continue");
+  }
+}*/
+
+int isBusyWith(pid_t pid, int status) {
+  if(pid==0 || errno==ECHILD) return -1;
+  if(WIFSTOPPED(status) || WIFEXITED(status) || WIFSIGNALED(status)) return 0;
+  return 1;
+}
+
+int indexOfBgJob(pid_t pid) {
+  bgJob *b;
+  int i=1;
+  for(b=bgJobs; b!=NULL; b=b->next, i++) {
+    if(b->pid == pid) return i;
+  }
+  return 0;
+}
+
+pid_t pidOfBgJob(int i) {
+  bgJob *b;
+  pid_t p=-1;
+  for(b=bgJobs; b!=NULL; b=b->next, i--) {
+    if(i==1 && b!=NULL) return b->pid;
+  }
+  return p;
+}
+
+int addBgJob(pid_t pid, char * line) {
+  bgJob *b;
+  int i = 1;
+  if(bgJobs == NULL) {
+    b = malloc(sizeof(bgJob));
+    b->next = NULL;
+    b->pid = pid;
+    b->line = line;
+    bgJobs = b;
+    return 1;
+  } else {
+    for(b=bgJobs; b!=NULL; b=b->next, i++) {
+      if(b->next == NULL) {
+        bgJob *jb;
+        jb = malloc(sizeof(bgJob));
+        jb->next = NULL;
+        jb->pid = pid;
+        jb->line = line;
+        b->next = jb;
+        return i+1;
+      }
+    }
+  }
+  return 0;
+}
+
 int exec(int fd[2], struct cmd *line, int bg) {
   pid_t pid;
   char buffer[UCHAR_MAX];
   int isBuiltin;
 
-  if((isBuiltin = notBuiltIn(line))<=0) {
+  if((isBuiltin = notBuiltIn(line->argv))<=0) {
     return isBuiltin;
   }
+  /*
+  void fgJjob(Jjob *j, int c) {
+    tcsetpgrp(_term, j->pgid);
+    if(c) {
+      tcsetattr(_term, TCSADRAIN, &j->tmodes);
+      if(kill(- j->pgid, SIGCONT) < 0) perror("SIGCONT error cannot continue");
+    }
+    waitJjob(j);
+    tcsetpgrp(_term, _pgid);
+    tcgetattr(_term, &j->tmodes);
+    tcsetattr(_term, TCSADRAIN, &_tmodes);
+  }
+
+  void bgJjob(Jjob *j, int c) {
+    if(c) {
+      if(kill(- j->pgid, SIGCONT) < 0) perror("SIGCONT error cannot continue");
+
+    }
+  }
+
+   */
   if (bg == 1) {
  	 signal(SIGCHLD, chldHandler);
   } else {
-	signal(SIGCHLD, pass);
+	 signal(SIGCHLD, SIG_DFL);
  }
 
   if((pid=fork()) == -1) return -1;
   if(pid == 0) {
     close(fd[0]);
+    if(bg) {
+      //close(STDOUT_FILENO);
+      int devnull = open("/dev/null",O_WRONLY | O_CREAT, 0666);
+      dup2(devnull, STDIN_FILENO);
+      dup2(devnull, STDOUT_FILENO);
+
+    } else {
+      dup2(fd[0], STDIN_FILENO);
+      dup2(fd[0], STDOUT_FILENO);
+    }
     //signal(SIGINT, ExecHandler);
     return execvp(line->argv[0], (char * const *)line->argv);
   } else {
     close(fd[1]);
-    read(fd[0], buffer, sizeof(buffer));
+    if(!bg) read(fd[0], buffer, sizeof(buffer));
+    else {
+      int ind = addBgJob(pid, line->argv[0]);
+      printf("[%d] %d \t%s\n", ind, pid, line->argv[0]);
+    }
   }
-  if(bg == 0) wait(&pid);
+  //if(bg == 0) wait(&pid);
+  if(bg == 0) {
+    pid_t cpid;
+    int status;
+    tcsetpgrp(_term, _pgid);
+    do {
+      cpid=waitpid(WAIT_ANY, &status, WUNTRACED);
+    } while(isBusyWith(pid, status));
+    tcsetattr(_term, TCSADRAIN, &_tmodes);
+  } else {
+    //if(kill(- pid, SIGCONT) < 0) perror("SIGCONT error cannot continue");
+  }
   return pid;
 }
 
@@ -362,17 +614,18 @@ void prompt() {
 }
 
 
-void putErr(char * err) {
+int putErr(char * err) {
   putstr(" [100m[91m");
-  putstr(" ⚠  [47m[90m");
+  putstr(" ⚠  [47m[90m ");
   putstr(err);
   putstr(" [37m[49m ");
   _putchar('\n');
+  return 0;
 }
 
 /* BUILT IN FUNCTIONS */
 
-char * _DEFINED_FUNCTIONS[] = {"cd", "env", "eval", "exit", "help", "wait",0};
+char * _DEFINED_FUNCTIONS[] = {"cd", "env", "end", "eval", "exit", "help","jobs", "tg", "wait",0};
 
 int cd(char **args) {
   char	*home;
@@ -384,6 +637,72 @@ int cd(char **args) {
     return changeDir(args[1]);
   }
   return 1;
+}
+
+int tg(char **args) {
+  if(!args[1] || strcmp(args[1], "--help") == 0) {
+    putstr("[47m[90m ");
+    putstr("tg");
+    putstr(" [37m[49m ");
+    putstr("<entry> Toggle shell modules. Try --list to display them.");
+    _putchar('\n');
+  } else if (strcmp(args[1], "-l") == 0 || strcmp(args[1], "--list") == 0 ) {
+    for (size_t i = 0; _DEFINED_MODULES[i]; i++) {
+      if(isModuleActive(_DEFINED_MODULES[i])) putstr(" [47m[90m+[37m[49m ");
+      else putstr(" [47m[90m-[37m[49m ");
+      putstr(_DEFINED_MODULES[i]);
+      putstr("\n");
+      /* code */
+    }
+  } else if (isModuleActive(args[1])>=0) {
+    toggleModule(args[1]);
+    if(isModuleActive(args[1])) printf("[47m[90m %s [37m[49m have been enabled.\n", args[1]);
+    else printf("[47m[90m %s [37m[49m have been disabled.\n", args[1]);
+  } else {
+    putErr("Module not found. Try --list to display them.");
+  }
+  /*int SHOW_CWD = 1;
+  int SHOW_NAME = 1;
+  int SHOW_GIT = 0;
+  */
+  return 0;
+}
+
+int endJob(char **args) {
+  if(!args[1] || strcmp(args[1], "--help") == 0) {
+    putstr("[47m[90m ");
+    putstr("end");
+    putstr(" [37m[49m ");
+    putstr("<num> End the given job.");
+    _putchar('\n');
+  } else {
+    bgJob *b;
+    int i = ft_atoi(args[1]);
+    if(i>0) {
+      i = pidOfBgJob(i);
+      return (i<1)?(putErr("Job not found")):(!killJob(i));
+    } else putErr("Invalid option");
+  }
+  return 0;
+}
+
+int showBgJobs() {
+  bgJob *b;
+  int i = 1;
+  updateJobs();
+  if(bgJobs == NULL) return putErr("No running jobs");
+  putstr("[47m[90m ");
+  putstr("Jobs");
+  putstr(" [37m[49m ");
+  putstr("Current running jobs :");
+  _putchar('\n');
+  for(b=bgJobs; b!=NULL; b=b->next, i++) {
+    putstr("[47m[90m ");
+    printf("%d [37m[49m ", i);
+    //printf("%s\t%d\n", b->line , b->pid);
+    printf("\t%d\n", b->pid);
+  }
+  return 0;
 }
 
 int _wait(char **args) {
@@ -456,13 +775,16 @@ int help(void) {
 
 /* ENDS HERE */
 
-int notBuiltIn(struct cmd *line) {
-  if(strcmp(line->argv[0], "exit") == 0) return -1;
-  else if(strcmp(line->argv[0], "env") == 0) return (printenv());
-  else if(strcmp(line->argv[0], "help") == 0) return (help());
-  else if(strcmp(line->argv[0], "cd") == 0) return (cd(line->argv));
-  else if(strcmp(line->argv[0], "wait") == 0) return (_wait(line->argv));
-  else if(strcmp(line->argv[0], "eval") == 0) return (_eval(line->argv));
+int notBuiltIn(char **argv) {
+  if(strcmp(argv[0], "exit") == 0) return -1;
+  else if(strcmp(argv[0], "env") == 0) return (printenv());
+  else if(strcmp(argv[0], "help") == 0) return (help());
+  else if(strcmp(argv[0], "cd") == 0) return (cd(argv));
+  else if(strcmp(argv[0], "jobs") == 0) return (showBgJobs());
+  else if(strcmp(argv[0], "end") == 0) return (endJob(argv));
+  else if(strcmp(argv[0], "wait") == 0) return (_wait(argv));
+  else if(strcmp(argv[0], "tg") == 0) return (tg(argv));
+  else if(strcmp(argv[0], "eval") == 0) return (_eval(argv));
   return 1;
 }
 
@@ -479,7 +801,7 @@ int processOne(char cmd[]) {
   pipes = strtok(cmd, "|");
   while (pipes != NULL && i+1<UCHAR_MAX)
   {
-    printf("%s\n", pipes);
+    //printf("%s\n", pipes);
     strncat(pipes, "\0", sizeof(pipes)+1);
     pipes = strtok(NULL, "|");
   }
@@ -552,6 +874,8 @@ int readInput() {
   input[i] = '\0';
   //testSub(input);
   //printf("%s\n", input);
+  //runJjob(makeJjob(input), 1);
+  //return 0;
   return processOne(input);
 }
 
@@ -563,7 +887,20 @@ int main(int argc, char const *argv[], char **envp) {
   char *input;
   int rtrn;
 
+  _term = STDIN_FILENO;
+  _itty = isatty(_term);
+
+  _pgid = getpid();
+  if(setpgid(_pgid, _pgid) <0) {
+    perror("Could not create process group");
+    exit(1);
+  }
+
+  tcsetpgrp(_term, _pgid);
+  tcgetattr(_term, &_tmodes);
+
   //testSub("ls");
+  //testJob();
 
   signal(SIGINT, handler);
   while(run) {
@@ -571,6 +908,6 @@ int main(int argc, char const *argv[], char **envp) {
     rtrn = readInput();
     if(rtrn<0) run=0;
   }
-
+  terminate();
   return 0;
 }
